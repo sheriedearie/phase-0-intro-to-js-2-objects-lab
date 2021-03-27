ba8099fac2859cff717bfe019ba4bec22cbf8a2a
Sheridan <sheridanmaustad@gmail.com>
2021-03-27 06:07:00 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: '123 somewhere street'
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+//accepts an employee object and a key and returns an employee
+//object wwithout the key value pair without affecting the employee
+function  deleteFromEmployeeByKey(employee, key){
+    //creating an newEmployee and copying it from employee
+    const newEmployee = {...employee};
+    //deletes a key value pair from newEmployee object
+    delete newEmployee[key];
+    //returning the newEmployee without the key value pair
+    return newEmployee;
+}
+//does the same thing as the previous function while mutating employee
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
